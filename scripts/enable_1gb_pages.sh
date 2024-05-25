0a550ad70cbe635efafb2086c6f26df51b3b8738
Steve Peters <25870521+stephenjonpeters@users.noreply.github.com>
2024-05-25 12:32:00 -0600

    automerge

--- scripts/enable_1gb_pages.sh
+++ scripts/enable_1gb_pages.sh
@@ -2,11 +2,11 @@
 
 # https://xmrig.com/docs/miner/hugepages#onegb-huge-pages
 
-sysctl -w vm.nr_hugepages=$(nproc)
+sysctl -w vm.nr_hugepages=2048
 
 for i in $(find /sys/devices/system/node/node* -maxdepth 0 -type d);
 do
-    echo 3 > "$i/hugepages/hugepages-1048576kB/nr_hugepages";
+    echo 2 > "$i/hugepages/hugepages-1048576kB/nr_hugepages";
 done
 
 echo "1GB pages successfully enabled"
