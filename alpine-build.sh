c2ac5329486dbb4d6b4504c9e94965b5ec710978
Steve Peters <25870521+stephenjonpeters@users.noreply.github.com>
2024-05-28 21:20:50 -0600

    automerge

--- alpine-build.sh
+++ alpine-build.sh
@@ -4,7 +4,63 @@ read -p "wallet: " WALLET
 PROCS=$(nproc)
 THREADS=$(($PROCS/2))
 
+#https://wiki.alpinelinux.org/wiki/Alpine_setup_scripts#setup-sshd
+setup-alpine
+
+apk add util-linux pciutils hwdata-pci usbutils hwdata-usb coreutils binutils findutils grep iproute2 vim wget plocate git ufw 
+
+
+#https://pkgs.alpinelinux.org/package/edge/main/x86_64/linux-firmware-radeon
+apk install linux-firmware-radeon
+
+#https://pkgs.alpinelinux.org/package/edge/main/x86_64/linux-firmware-amdgpu
+#apk install linux-firmware-amdgpu
+
 #https://xmrig.com/docs/miner/build/alpine
+
+sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="msr.allow_writes=on"/'  /etc/default/grub
+update-grub
+
+git config --global user.email "stephenjonpeters@icloud.com"
+git config --global user.name "Steve Peters"
+
+cp /etc/hosts /etc/hosts.$TS
+cat <<EOF>> /etc/hosts
+192.168.0.10 xmrig1
+192.168.0.20 xmrig2
+192.168.0.30 xmrig3
+192.168.0.40 xmrig4
+192.168.0.50 xmrig5
+192.168.0.60 xmrig6
+EOF
+
+cp ~/.bashrc ~/.bashrc.$TS
+cat <<EOF>> ~/.bashrc
+VISUAL="vim" ; export VISUAL
+EDITOR="\$VISUAL" ; export EDITOR
+EOF
+
+cat <<EOF> ~/.vimrc
+set mouse=v
+set number
+EOF
+
+source .bashrc
+
+cp /etc/security/limits.conf /etc/security/limits.conf.$TS
+cat <<EOF>>  /etc/security/limits.conf
+echo root hard nofile 1048576
+echo root soft nofile 1048576
+EOF
+
+cp  /etc/sysctl.conf  /etc/sysctl.conf.$TS
+cat <<EOF> /etc/sysctl.conf
+vm.nr_hugepages=1024
+kernel.shmmax = 3254779904
+vm.hugetlb_shm_group = 0
+vm.min_free_kbytes = 112640
+EOF
+
 
 cd /root/ 
 rm -rf xxxmrig
@@ -12,6 +68,8 @@ rm -f /opt/xmrig/xmrig
 rm -f /opt/xmrig/xmr.json
 rm -f /opt/xmrig/config.json
 mkdir -p /opt/xmrig /var/lib/xmrig
+
+apk add git make cmake libstdc++ gcc g++ automake libtool autoconf linux-headers
 
 cp xxxmrig/src/config.json /opt/xmrig
 mkdir xxxmrig/build && cd xxxmrig/scripts
